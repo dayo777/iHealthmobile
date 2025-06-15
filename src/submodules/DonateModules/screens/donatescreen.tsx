bacd7f8032008f8a74a2a45f69d8739cf8da5642
Egbetayo Damilola <76609799+Hezhykeyhel@users.noreply.github.com>
2025-06-15 13:52:41 +0100

    fix: re-added donateModules submodule at new path

---
+++ src/submodules/DonateModules/screens/donatescreen.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@/src/components/Restyle";
+
+export default function DonateScreen() {
+  return (
+    <Box flex={1} justifyContent="center" alignItems="center">
+      <Text textAlign="center" variant="header">
+        Donate Screen
+      </Text>
+    </Box>
+  );
+}
